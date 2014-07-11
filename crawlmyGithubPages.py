edaa294de935b27ce104dba95cd828496ae523ec
balzer82 <balzer82@gmail.com>
2014-07-11 11:03:20 +0200

    Aktualisiert und Google Analytics Code

--- crawlmyGithubPages.py
+++ crawlmyGithubPages.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
+# <headingcell level=1>
+
+# Automatically Create an Overview-Website out of 'User' Github Repos
+
 # <codecell>
 
 import requests
@@ -47,6 +51,18 @@ prestring="""
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46744695-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </head>
 <body>
 
